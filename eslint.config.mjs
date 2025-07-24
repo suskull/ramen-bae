014b756d0e5ce574f403d3f03f91238067afc23f
hieuvv <xoaixanh.muoii95@gmail.com>
2025-07-24 13:51:36 +0700

    feat(front-end): config tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["src/app/api/learning/**/*", "Learning/**/*"],
+  },
 ];
 
 export default eslintConfig;
